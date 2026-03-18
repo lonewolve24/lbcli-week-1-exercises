296a41d94288c31edd05e797289515f0bd65136a
Abdul Muizz <azikumapayi@gmail.com>
2026-03-18 23:29:50 +0000

    submission of each coomad for each file

--- submission/04.sh
+++ submission/04.sh
@@ -1,1 +1,2 @@
 # Create a wallet with the name "builderswallet".
+bitcoin-cli -named createwallet wallet_name="builderswallet" descriptors=true
--- submission/09.sh
+++ submission/09.sh
@@ -1,1 +1,2 @@
 # Generate a Taproot address.
+bitcoin-cli -regtest getnewaddress -addresstype bech32
